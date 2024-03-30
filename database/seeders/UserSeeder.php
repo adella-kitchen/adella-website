3251d1106e4265d7bccb1a964ea897c2a233a151
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2024-03-30 23:06:58 +0700

    add seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Nico Flassy',
+            'email' => 'nico@gmail.com',
+            'password' => bcrypt('nico123'),
+            'role' => 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Mas Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123'),
+            'role' => 'admin',
+        ]);
+        
+        DB::table('users')->insert([
+            'name' => 'Mas Customer',
+            'email' => 'customer@gmail.com',
+            'password' => bcrypt('customer123'),
+            'role' => 'customer',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Mas Owner',
+            'email' => 'owner@gmail.com',
+            'password' => bcrypt('owner123'),
+            'role' => 'owner',
+        ]);
+    }
+}
