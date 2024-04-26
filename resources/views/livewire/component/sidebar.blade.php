8a5c7d3c67909470549d7c69ddb7b8ac35999767
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2024-04-26 09:40:21 +0700

    updating logo adella kitchen on sidebar

--- resources/views/livewire/component/sidebar.blade.php
+++ resources/views/livewire/component/sidebar.blade.php
@@ -3,7 +3,7 @@
     aria-label="Sidebar">
     <div class="h-full px-3 py-6 overflow-y-auto bg-white border dark:bg-gray-800">
         <div class="w-full flex flex-col justify-center text-center items-center mb-10 gap-3">
-            <div class="w-[50px] h-[50px] bg-red"></div>
+            <img src="/img/logo/ADELA_ORIGINAL.png" alt="" class="w-[80px]">
             <p class="font-semibold text-[20px]">Adella Kitchen</p>
         </div>
         <ul class="space-y-2 font-medium">
