4e99606b8f8498197616df3fdb301859fb03da58
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-05-28 00:20:16 +0700

    create api for get data user

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/menu', [MenuController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/menu/{id}', [MenuController::class, 'show'])->middleware('auth:sanctum');
 Route::get('/menu/kategori/{category}', [MenuController::class, 'getCategory'])->middleware('auth:sanctum');
+Route::get('/user/{id}', [AuthController::class, 'getUser'])->middleware('auth:sanctum');
+Route::get('/promo', [ContentPromoController::class, 'getPromo'])->middleware('auth:sanctum');
 
 
 // api cart
 Route::get('/cart', [CartAPIController::class, 'index'])->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
